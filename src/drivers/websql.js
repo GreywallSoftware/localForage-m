0582c75249d633b524b337fbe5e20456b913aa23
Anubhav <anubhavs19@gmail.com>
2022-08-17 15:27:07 +0530

    iterate functionality changed

--- src/drivers/websql.js
+++ src/drivers/websql.js
@@ -172,43 +172,47 @@ function iterate(iterator, callback) {
                 var dbInfo = self._dbInfo;
 
                 dbInfo.db.transaction(function(t) {
-                    tryExecuteSql(
-                        t,
-                        dbInfo,
-                        `SELECT * FROM ${dbInfo.storeName}`,
-                        [],
-                        function(t, results) {
-                            var rows = results.rows;
-                            var length = rows.length;
+                    (function executeSql(page = 0) {
+                        var results;
+                        tryExecuteSql(
+                            t,
+                            dbInfo,
+                            `SELECT * FROM ${dbInfo.storeName} LIMIT ${page *
+                                20}, 20`,
+                            [],
+                            function(t, results) {
+                                var rows = results.rows;
+                                var length = rows.length;
 
-                            for (var i = 0; i < length; i++) {
-                                var item = rows.item(i);
-                                var result = item.value;
+                                for (var i = 0; i < length; i++) {
+                                    var item = rows.item(i);
+                                    var result = item.value;
 
-                                // Check to see if this is serialized content
-                                // we need to unpack.
-                                if (result) {
-                                    result = dbInfo.serializer.deserialize(
-                                        result
-                                    );
+                                    // Check to see if this is serialized content
+                                    // we need to unpack.
+                                    if (result) {
+                                        result = dbInfo.serializer.deserialize(
+                                            result
+                                        );
+                                    }
+
+                                    result = iterator(result, item.key, i + 1);
+
+                                    // void(0) prevents problems with redefinition
+                                    // of `undefined`.
+                                    if (result !== void 0) {
+                                        resolve(result);
+                                        return;
+                                    }
                                 }
 
-                                result = iterator(result, item.key, i + 1);
-
-                                // void(0) prevents problems with redefinition
-                                // of `undefined`.
-                                if (result !== void 0) {
-                                    resolve(result);
-                                    return;
-                                }
+                                resolve();
+                            },
+                            function(t, error) {
+                                reject(error);
                             }
-
-                            resolve();
-                        },
-                        function(t, error) {
-                            reject(error);
-                        }
-                    );
+                        );
+                    })();
                 });
             })
             .catch(reject);
